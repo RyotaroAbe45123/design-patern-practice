20d70a3563748bb3fe9b8b758abdb497d328d4fd
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-12 21:52:20 +0900

    add: main script

---
+++ python/TemplateMethod/main.py
@@ -0,0 +1,14 @@
+from concrete_class1 import ConcreteA
+from concrete_class2 import ConcreteB
+
+
+def main():
+    concreteA = ConcreteA()
+    concreteB = ConcreteB()
+
+    concreteA.print()
+    concreteB.print()
+
+
+if __name__ == "__main__":
+    main()
