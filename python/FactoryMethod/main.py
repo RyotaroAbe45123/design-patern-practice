45702ed84df4b8b9c7852ebf261b49a7f06d49b1
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-20 04:21:58 +0900

    add: main part

--- python/FactoryMethod/main.py
+++ python/FactoryMethod/main.py
@@ -1,6 +1,15 @@
-def main():
-    pass
+from concrete.concrete_factory import ConcreteFactory
 
+
+def main():
+    factory = ConcreteFactory()
+    hoge = factory.create("hoge")
+    hoge.print()
+    fuga = factory.create("fuga")
+    fuga.print()
+    # print(dir(factory))
+    # factory.__private()
+    # factory._Factory__private()
 
 if __name__ == "__main__":
     main()
