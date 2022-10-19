670458f2391d0de5c502fc7a905842bcdf28b351
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-20 03:44:23 +0900

    update: args

--- python/FactoryMethod/framework/factory.py
+++ python/FactoryMethod/framework/factory.py
@@ -4,14 +4,14 @@ from .product import Product
 
 
 class Factory(ABC):
-    def create(self):
-        product = self.create_product()
+    def create(self, product_name: str):
+        product = self.create_product(product_name)
         self.register_product(product)
 
     @abstractmethod
-    def create_product(self) -> Product:
+    def create_product(self, product_name: str) -> Product:
         pass
 
     @abstractmethod
-    def register_product(self) -> None:
+    def register_product(self, product: Product) -> None:
         pass
