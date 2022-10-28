5220c2e5d5b8e378502b6938a2e1a85fb218df38
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-28 09:01:49 +0900

    update: main scritp

--- python/Singleton/main.py
+++ python/Singleton/main.py
@@ -1,3 +1,14 @@
-class A:
-    pass
-print(type(A))
+from singleton import Singleton
+
+
+def main() -> None:
+    a = Singleton(value=1)
+    print(f"a: {a.__value}")
+
+    b = Singleton(value=2)
+    print(f"a: {a.__value}")
+    print(f"b: {b.__value}")
+
+
+if __name__ == "__main__":
+    main()
