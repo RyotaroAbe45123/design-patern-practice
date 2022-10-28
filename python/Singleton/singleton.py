06d62e6d94f6307ddbe0a21b1e1530df84c30698
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-28 19:32:22 +0900

    update: Singleton

--- python/Singleton/singleton.py
+++ python/Singleton/singleton.py
@@ -5,7 +5,15 @@ class Base(object):
         return self._instance
 
 
-class Singleton(Base):
+class _Singleton(Base):
     def __init__(self, value: int):
         self._value = value
         self.__value = value
+
+
+class Singleton:
+    __instance = None
+    def __new__(self, *args, **kwargs):
+        if Singleton.__instance is None:
+            Singleton.__instance = super().__new__(self)
+        return Singleton.__instance
