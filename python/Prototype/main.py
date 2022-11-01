bafac01405bb5f2b6b3abc22a4a2854ed3727619
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-01 12:58:13 +0900

    update: show num of obj

--- python/Prototype/main.py
+++ python/Prototype/main.py
@@ -10,8 +10,10 @@ def main():
     teacher.register("paper", paper)
 
     papers = teacher.create_many_crystals("paper")
+    print(len(papers))
     print(papers[0])
     print(papers[1])
+    print(papers[2])
 
 
 if __name__ == "__main__":
