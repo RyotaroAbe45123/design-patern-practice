badf9708a6d10eb701bdf8bf6f65aea394f0b529
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-23 03:47:32 +0900

    update: main

--- python/AbstractFactory/main.py
+++ python/AbstractFactory/main.py
@@ -1,15 +1,18 @@
 import sys
 
+from factory import Factory
 from hot_pot.pot import Pot
 from hot_pot.hot_pot import HotPot
-from mizutaki.mizutaki_factory import MizutakiFactory
+
 
 def main():
     assert len(sys.argv) != 1, "Argments Error"
     want_to_eat = sys.argv[1]
 
+    factory: Factory = Factory.get_factory(want_to_eat)
+    print(factory)
+
     hot_pot: HotPot = HotPot(Pot())
-    factory = MizutakiFactory()
     hot_pot.add_soup(factory.get_soup())
     hot_pot.add_protain(factory.get_protain())
     hot_pot.add_vegetables(factory.get_vegetables())
